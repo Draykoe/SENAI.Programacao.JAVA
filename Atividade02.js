006615f2062283196c442d84b2c509ee77b3cb4e
Roberto <roberto.morita@hotmail.com>
2021-09-13 15:47:59 -0300

    Atividade02-JAVA-Correcao

--- Atividade02.js
+++ Atividade02.js
@@ -3,5 +3,5 @@ if (listadealunos == 0, 2, 4, 6, 8)
     console.log("Os alunos 0, 2, 4, 6 e 8 são PAR");
     if (listadealunos == 1, 3, 5, 7)
     console.log("Os alunos 1, 3, 5, 7 são IMPAR");
-    if (listadealunos == 0)
-    console.log("O Aluno é 0");
+    if (listadealunos == 0);
+    console.log("O Aluno 0 é 0");
